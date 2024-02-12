89c08a7f32d7274997dd181f7dad59ecbb320e8b
Ryokenka <remi.guerin99@gmail.com>
2024-02-12 17:07:15 +0100

    Update to ClientDao

--- src/main/java/com/epf/rentmanager/dao/ClientDao.java
+++ src/main/java/com/epf/rentmanager/dao/ClientDao.java
@@ -12,37 +12,65 @@ import java.util.List;
 import java.util.Optional;
 
 import com.epf.rentmanager.persistence.ConnectionManager;
+import com.epf.rentmanager.models.Client;
 
 public class ClientDao {
-	
 	private static ClientDao instance = null;
-	private ClientDao() {}
+
+	private ClientDao() {
+	}
+
 	public static ClientDao getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new ClientDao();
 		}
 		return instance;
 	}
-	
+
 	private static final String CREATE_CLIENT_QUERY = "INSERT INTO Client(nom, prenom, email, naissance) VALUES(?, ?, ?, ?);";
 	private static final String DELETE_CLIENT_QUERY = "DELETE FROM Client WHERE id=?;";
 	private static final String FIND_CLIENT_QUERY = "SELECT nom, prenom, email, naissance FROM Client WHERE id=?;";
 	private static final String FIND_CLIENTS_QUERY = "SELECT id, nom, prenom, email, naissance FROM Client;";
-	
+
 	public long create(Client client) throws DaoException {
-		return 0;
-	}
-	
-	public long delete(Client client) throws DaoException {
-		return 0;
-	}
+		try {
+			Connection connection = ConnectionManager.getConnection();
+			PreparedStatement ps =
+					connection.prepareStatement(CREATE_CLIENT_QUERY);
 
-	public Client findById(long id) throws DaoException {
-		return new Client();
-	}
+			ps.setString(1, client.getNom());
+			ps.setString(2, client.getPrenom());
+			ps.setString(3, client.getEmail());
+			ps.setDate(4, Date.valueOf(client.getNaissance()));
 
-	public List<Client> findAll() throws DaoException {
-		return new ArrayList<Client>();
-	}
+			ps.executeUpdate();
 
+			// Retrieve the generated keys to get the inserted client's ID
+			//ResultSet generatedKeys = ps.getGeneratedKeys();
+			//long clientId = -1;
+			//if (generatedKeys.next()) {
+				//clientId = generatedKeys.getLong(1);
+			//}
+			ps.close();
+			connection.close();
+
+			//return clientId;
+		} catch (SQLException e) {
+			throw new DaoException("Une erreur est survenue lors de la création du client", e);;
+		}
+
+
+		public long delete (Client client) throws DaoException {
+			return 0;
+		}
+
+		public Client findById ( long id) throws DaoException {
+			return new Client();
+		}
+
+		public List<Client> findAll () throws DaoException {
+			return new ArrayList<Client>();
+		}
+
+	}
 }
